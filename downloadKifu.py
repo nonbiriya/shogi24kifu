29873e6d6d3d07f3e0c07678d31661bf7341eb1c
hachimitsu8823 <hachimitsu8823@gmail.com>
2019-03-03 08:07:12 +0900

    バグ修正

--- downloadKifu.py
+++ downloadKifu.py
@@ -8,24 +8,30 @@ class downloadKifu:
         iniinfo = Shogi24Lib.inifile()
         self._shogi24login = None
         if iniinfo.readIniFile():
-            self._shogi24login = Shogi24Lib.login(iniinfo.getUserName,iniinfo.getUserPass)
+            self._shogi24login = Shogi24Lib.login(iniinfo.getUserName(),iniinfo.getUserPass())
+            self.setSearchId(iniinfo.getUserName())
         else:
             username = Shogi24Lib.defaultInput.inputID()
             password = Shogi24Lib.defaultInput.inputPass()
             self._shogi24login = Shogi24Lib.login(username,password)
+            self.setSearchId(username)
         self._shogi24login.doLogin()
         return self._shogi24login.getSession()
+    
+    #検索対象のIDを設定
+    def setSearchId(self,id):
+        self._searchId = id
 
     #検索して、ログイン者のIDを取得する
     def setUserId(self,session):
-        self._searchUser = Shogi24Lib.searchUser(session,self._shogi24login.getUserName())
+        self._searchUser = Shogi24Lib.searchUser(session,self._searchId)
         self._searchUser.search()
-        return self._searchUSer.getMatchId()
+        return self._searchUser.getMatchId()
         
 
     #スクレイピング
     def doScraping(self,session,userID):
-        kifulistpage = Shogi24Lib.kifuListPage()
+        kifulistpage = Shogi24Lib.kifuListPage(session)
         kifulistpage.setId(userID)
         kifulistpage.getListPage()
         return kifulistpage.scrapingListPage()
@@ -33,11 +39,11 @@ class downloadKifu:
 
     #リストから棋譜をファイル出力
     def makeKifuFile(self,resultList):
-        shogi24Lib.kifu(resultList)
+        Shogi24Lib.kifu(resultList)
 
 if __name__ == "__main__":
     dwnkifu = downloadKifu()
     session = dwnkifu.login()
     userId = dwnkifu.setUserId(session)
     resultList = dwnkifu.doScraping(session,userId)
-    makeKifuFile(resultList)
+    dwnkifu.makeKifuFile(resultList)
